4f63c4ba96af4639f7c5c7d45a67ca9e4bab445f
yigitokar <okar.yigit@gmail.com>
2024-06-27 19:59:16 +0300

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#A61128',
+          500: '#7B0D1E',
+          600: '#37060D',
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
+export default config;
